3ad1020e815a233c4418340433add245e97d177c
KeithSRoberts <robertsskeith@gmail.com>
2020-04-08 15:11:54 -0700

    Finished extra credit 1, 2, 3

--- app/src/main/java/com/example/dotify/MainActivity.kt
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dotify
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.*
 
 
@@ -14,10 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Hide title bar within activity
-        try {
-            this.supportActionBar!!.hide()
-        } catch (e: NullPointerException) {}
         setContentView(R.layout.activity_main)
 
         // Set initial random play count
@@ -25,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val playNums: TextView = findViewById(R.id.textView3)
         playNums.text = "$randomPlays plays";
 
+        // Change user functionality
         val changeUserButton: Button = findViewById(R.id.button);
         val usernameText: TextView = findViewById(R.id.username);
         val editText: EditText = findViewById(R.id.editText);
@@ -43,20 +42,38 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Next button functionality
         val nextButton: ImageView = findViewById(R.id.imageView4);
         nextButton.setOnClickListener() {
             Toast.makeText(applicationContext,"Skipping to next track", Toast.LENGTH_SHORT).show();
         }
 
+        // Previous button functionality
         val prevButton: ImageView = findViewById(R.id.imageView2);
         prevButton.setOnClickListener() {
             Toast.makeText(applicationContext,"Skipping to previous track", Toast.LENGTH_SHORT).show();
         }
 
+        // Play button functionality
         val playButton: ImageView = findViewById(R.id.imageView3);
         playButton.setOnClickListener() {
             randomPlays++;
             playNums.text = "$randomPlays plays";
+        }
+
+        // Cover image long press functionality
+        val parentCL: ConstraintLayout = findViewById(R.id.constraint);
+        val coverImage: ImageView = findViewById(R.id.imageView);
+        coverImage.setOnLongClickListener() {
+            val rnd = Random()
+            val color: Int = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+            for (i in 0 until parentCL.childCount) {
+                if (parentCL.getChildAt(i) is TextView) {
+                    val tv = parentCL.getChildAt(i) as TextView
+                    tv.setTextColor(color)
+                }
+            }
+            true
         }
     }
 }
