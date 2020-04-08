f91e509c9adbc2c01f3cac22073dd5dc4162bef5
KeithSRoberts <robertsskeith@gmail.com>
2020-04-08 03:59:22 -0700

    Added functionality for change user button and validation

--- app/src/main/java/com/example/dotify/MainActivity.kt
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.dotify
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -25,6 +24,24 @@ class MainActivity : AppCompatActivity() {
         var randomPlays: Int = Random().nextInt(max - min + 1) + min;
         val playNums: TextView = findViewById(R.id.textView3)
         playNums.text = "$randomPlays plays";
+
+        val changeUserButton: Button = findViewById(R.id.button);
+        val usernameText: TextView = findViewById(R.id.username);
+        val editText: EditText = findViewById(R.id.editText);
+        editText.visibility = View.GONE;
+        changeUserButton.setOnClickListener() {
+            if (changeUserButton.text == "Change User") {
+                changeUserButton.text = "Apply";
+                editText.setText("");
+                usernameText.visibility = View.GONE;
+                editText.visibility = View.VISIBLE;
+            } else if (changeUserButton.text == "Apply" && editText.text.toString() != "") {
+                changeUserButton.text = "Change User";
+                usernameText.text = editText.text;
+                editText.visibility = View.GONE;
+                usernameText.visibility = View.VISIBLE;
+            }
+        }
 
         val nextButton: ImageView = findViewById(R.id.imageView4);
         nextButton.setOnClickListener() {
