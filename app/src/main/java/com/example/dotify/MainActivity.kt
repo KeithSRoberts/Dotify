9e3c111164701b4d5fa5c97e8bedbb93f167792e
KeithSRoberts <robertsskeith@gmail.com>
2020-04-08 03:26:07 -0700

    Added button functionality and scrollable view, hide title bar in main screen

--- app/src/main/java/com/example/dotify/MainActivity.kt
+++ app/src/main/java/com/example/dotify/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.dotify
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -10,14 +12,34 @@ class MainActivity : AppCompatActivity() {
 
     private val min = 0;
     private val max = 1000;
-    private val randomPlays: Int = Random().nextInt(max - min + 1) + min;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Hide title bar within activity
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {}
         setContentView(R.layout.activity_main)
 
+        // Set initial random play count
+        var randomPlays: Int = Random().nextInt(max - min + 1) + min;
         val playNums: TextView = findViewById(R.id.textView3)
-        val text = "$randomPlays plays";
-        playNums.text = text;
+        playNums.text = "$randomPlays plays";
+
+        val nextButton: ImageView = findViewById(R.id.imageView4);
+        nextButton.setOnClickListener() {
+            Toast.makeText(applicationContext,"Skipping to next track", Toast.LENGTH_SHORT).show();
+        }
+
+        val prevButton: ImageView = findViewById(R.id.imageView2);
+        prevButton.setOnClickListener() {
+            Toast.makeText(applicationContext,"Skipping to previous track", Toast.LENGTH_SHORT).show();
+        }
+
+        val playButton: ImageView = findViewById(R.id.imageView3);
+        playButton.setOnClickListener() {
+            randomPlays++;
+            playNums.text = "$randomPlays plays";
+        }
     }
 }
